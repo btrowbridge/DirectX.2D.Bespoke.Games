7c98a44d36551c7f970cf13fba8cf932a9c1db5c
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-08-29 10:54:47 -0400

    Bouncing Logo Game commit

---
+++ Native/Framework/source/Samples/BouncingLogo/BouncingLogoGame.cpp
@@ -0,0 +1,60 @@
+#include "pch.h"
+#include "BouncingLogoGame.h"
+
+using namespace std;
+using namespace DirectX;
+using namespace Library;
+
+namespace BouncingLogo
+{
+	const XMVECTORF32 BouncingLogoGame::BackgroundColor = Colors::CornflowerBlue;
+
+	BouncingLogoGame::BouncingLogoGame(function<void*()> getWindowCallback, function<void(SIZE&)> getRenderTargetSizeCallback) :
+		Game(getWindowCallback, getRenderTargetSizeCallback)
+	{
+	}
+
+	void BouncingLogoGame::Initialize()
+	{
+		mKeyboard = make_shared<KeyboardComponent>(*this);
+		mComponents.push_back(mKeyboard);
+		mServices.AddService(KeyboardComponent::TypeIdClass(), mKeyboard.get());
+
+		Game::Initialize();
+	}
+
+	void BouncingLogoGame::Update(const GameTime &gameTime)
+	{
+		if (mKeyboard->WasKeyPressedThisFrame(Keys::Escape))
+		{
+			Exit();
+		}
+
+		Game::Update(gameTime);
+	}
+
+	void BouncingLogoGame::Draw(const GameTime &gameTime)
+	{
+		mDirect3DDeviceContext->ClearRenderTargetView(mRenderTargetView.Get(), reinterpret_cast<const float*>(&BackgroundColor));
+		mDirect3DDeviceContext->ClearDepthStencilView(mDepthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+		Game::Draw(gameTime);
+
+		HRESULT hr = mSwapChain->Present(1, 0);
+
+		// If the device was removed either by a disconnection or a driver upgrade, we must recreate all device resources.
+		if (hr == DXGI_ERROR_DEVICE_REMOVED || hr == DXGI_ERROR_DEVICE_RESET)
+		{
+			HandleDeviceLost();
+		}
+		else
+		{
+			ThrowIfFailed(hr, "IDXGISwapChain::Present() failed.");
+		}
+	}
+
+	void BouncingLogoGame::Exit()
+	{
+		PostQuitMessage(0);
+	}
+}
