5770485addaa1f99ec3cb20e8e69c7e47e819441
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-09-02 11:29:53 -0400

    Add player paddle

--- Native/Framework/source/Samples/BouncingLogo/PlayerPaddle.cpp
+++ Native/Framework/source/Samples/BouncingLogo/PlayerPaddle.cpp
@@ -7,55 +7,35 @@ using namespace std;
 using namespace Microsoft::WRL;
 using namespace DirectX;
 
-BouncingLogo::PlayerPaddle::PlayerPaddle(Library::Game & game) : Paddle(game)
-{
-}
+namespace BouncingLogo {
+	const int BouncingLogo::PlayerPaddle::mSpeed = 10;
 
-void BouncingLogo::PlayerPaddle::Initialize()
-{
-	// Load a texture
-	ComPtr<ID3D11Resource> textureResource;
-	wstring textureName = L"Content\\Textures\\PlayerPaddle.png";
+	PlayerPaddle::PlayerPaddle(Library::Game & game, std::shared_ptr<Library::KeyboardComponent> keyboard) : Paddle(game), mKeyboard(keyboard)
+	{
+	}
 
-	ThrowIfFailed(CreateWICTextureFromFile(mGame->Direct3DDevice(), textureName.c_str(),
-		textureResource.ReleaseAndGetAddressOf(), mTexture.ReleaseAndGetAddressOf()),
-		"CreateWICTextureFromFile() Failed.");
+	void PlayerPaddle::Initialize()
+	{
+		mVelocity.x = 0;
+		mVelocity.y = 0;
+		
+		mBounds.X = mTextureHalfSize.X;
+		mBounds.Y = mGame->Viewport().Height / 2;
+	}
 
-	ComPtr<ID3D11Texture2D> texture;
-	ThrowIfFailed(textureResource.As(&texture), "Invalid ID3D11 texture resource");
+	void PlayerPaddle::Update(const Library::GameTime & gameTime)
+	{
+		float elapsedTime = gameTime.ElapsedGameTimeSeconds().count();
 
-	mBounds = TextureHelper::GetTextureBounds(texture.Get());
-
-
-	Point mTextureHalfSize;
-	mTextureHalfSize.X = mBounds.Width / 2;
-	mTextureHalfSize.Y = mBounds.Height / 2;
-
-	auto& mViewport = mGame->Viewport();
-
-	Library::Rectangle viewportSize(static_cast<int>(mViewport.TopLeftX), static_cast<int>(mViewport.TopLeftY),
-		static_cast<int>(mViewport.Width), static_cast<int>(mViewport.Height));
-	Point center = viewportSize.Center();
-
-	mBounds.X = center.X - mTextureHalfSize.X;
-	mBounds.Y = center.Y - mTextureHalfSize.Y;
-
-	mVelocity.x = 0;
-	mVelocity.y = 0;
-
-	mSpriteBatch = make_unique<SpriteBatch>(mGame->Direct3DDeviceContext());
-}
-
-void BouncingLogo::PlayerPaddle::Update(const Library::GameTime & gameTime)
-{
-}
-
-void BouncingLogo::PlayerPaddle::Draw(const Library::GameTime & gameTime)
-{
-	XMFLOAT2 position(mBounds.X, mBounds.Y);
-
-	mSpriteBatch->Begin();
-	mSpriteBatch->Draw(mTexture.Get(), position);
-	mSpriteBatch->End();
+		if (mKeyboard->IsKeyDown(Keys::Up) && mBounds.Y - mTextureHalfSize.Y - mVelocity.y > 0)
+		{
+			mVelocity.y = mSpeed * elapsedTime;
+		}
+		else if (mKeyboard->IsKeyDown(Keys::Down) && mBounds.Y - mTextureHalfSize.Y - mVelocity.y < mGame->Viewport().Height)
+		{
+			mVelocity.y = -mSpeed * elapsedTime;
+		}
+	}
+	
 
 }
